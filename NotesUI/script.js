f391e666be8205f6eabe7d5938f3cd2dfe3091dd
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-15 14:54:19 +0300

    DELETE method added

--- NotesUI/script.js
+++ NotesUI/script.js
@@ -2,22 +2,39 @@ const saveButton = document.querySelector('#btnSave');
 const titleInput = document.querySelector('#title');
 const descriptionInput = document.querySelector('#description');
 const notesContainer = document.querySelector('#notes__container');
+const deleteButton = document.querySelector('#btnDelete');
+
 
 
 function clearForm() {
     titleInput.value = '';
     descriptionInput.value = '';
+    deleteButton.classList.add('hidden');
 }
 
 
+function displayNoteInForm(note) {
+    titleInput.value = note.title;
+    descriptionInput.value = note.description;
+    deleteButton.classList.remove('hidden');
+    deleteButton.setAttribute('data-id', note.id);
+}
+function getNoteById(id) {
+        fetch(`https://localhost:7054/api/notes/${id}`)
+        .then(data => data.json())
+        .then(response => displayNoteInForm(response));
+}
+
+function populateForm(id) {
+    getNoteById(id);
+}
 
 function addNote(title, description) {
-
     const body = {
         title: title,
         description: description,
         isVisible: true
-    };
+};
 
 
 fetch('https://localhost:7054/api/notes', {
@@ -35,31 +52,58 @@ fetch('https://localhost:7054/api/notes', {
 
 }
 
+
+
 function getAllNotes() {
-    fetch('https://localhost:7054/api/notes')
+    fetch(`https://localhost:7054/api/notes`)
     .then(data => data.json())
     .then(response => displayNotes(response));
 }
 
 function displayNotes(notes) {
-
     let allNotes = '';
 
 notes.forEach(note => {
     const noteElement = `
-                            <div class="note"> 
+                            <div class="note" data-id="${note.id}"> 
                             <h3>${note.title}</h3>
                             <p>${note.description}</p>
                             </div>
                               `;  
     allNotes += noteElement;
+    
    
 });
-    notesContainer.innerHTML = allNotes;
+    
+notesContainer.innerHTML = allNotes;
+
+    document.querySelectorAll('.note').forEach(note => {
+        note.addEventListener('click', function() {
+            populateForm(note.dataset.id);
+        });
+    });
 }
 
 
 getAllNotes();
 saveButton.addEventListener('click',function() {
     addNote(titleInput.value, descriptionInput.value);
-})
+});
+
+function deleteNote(id) {
+    fetch(`https://localhost:7054/api/notes/${id}`, {
+        method:'DELETE',
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    .then(response => {
+     clearForm();
+     getAllNotes();
+    });
+}
+
+deleteButton.addEventListener('click', function() {
+   const id = deleteButton.dataset.id;
+    deleteNote(id);
+});
